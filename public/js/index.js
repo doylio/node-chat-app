4480a5797ec809a7792298379ef14894b2ef33d2
Shawn Doyle <shawndoyl@gmail.com>
2019-02-20 16:09:23 -0500

    Setup socket.io

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+const socket = io();
+
+socket.on('connect', function() {
+    console.log("Connected to server");
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function(email) {
+    console.log(email);
+});
+
+socket.on('newMessage', function(message) {
+    console.log(message);
+});
+
+socket.emit('createMessage', {
+    to: 'Andrew',
+    text: "Thanks for the course, dude!"
+})
--- server/server.js
+++ server/server.js
@@ -1,16 +1,41 @@
 //Libraries
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-//Init Server
+//Init
 const publicPath = path.join(__dirname, '../public');
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 //Middleware
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('createEmail', newEmail => {
+        console.log('createEmail', newEmail);
+    });
+
+    socket.emit('newMessage', {
+        from: 'Sandy',
+        text: 'This is a new message'
+    });
+
+    socket.on('createMessage', message => {
+        console.log(message);
+    })
+});
+
+server.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
 
