e75d9ad89e5a8681b6e3145c83aee2181428991d
Shawn Doyle <shawndoyl@gmail.com>
2019-02-20 14:35:25 -0500

    First commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+//Librarie
+const path = require('path');
+const express = require('express');
+
+//Init Server
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.static(publicPath))
+
+app.listen(3000, () => console.log(`Listening on port ${port}`));
+
+
